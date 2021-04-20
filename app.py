6045033a33c2ef5c697fa6fd49a2960d442739ae
Emil Karlström <emil.karlstrom@gmail.com>
2021-04-20 10:45:46 +0200

    Backend boilerplate

---
+++ app.py
@@ -0,0 +1,13 @@
+import argparse
+from backend import create_app
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--ip', type=str, required=True,
+                help='The ip to run the Flask server on')
+parser.add_argument('--port', type=int, required=True,
+                help='The port to run the Flask server on')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    app = create_app(args.ip, args.port)
+    app.run()
---
+++ backend/backend_app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import jsonify
+
+BASE_DIR = os.path.dirname(__file__)
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
+def create_app(host, port):
+    app = App(host=host, port=port)
+    
+    @app.route('/')
+    def _home():
+        return render_template("home.html")
+
+    return app
+
+class App(Flask):
+    def __init__(self, host, port):
+        super().__init__(__name__)
+        self._host = host
+        self._port = port
+    
+    def run(self):
+        super().run(host=self._host, port=self._port)
+
