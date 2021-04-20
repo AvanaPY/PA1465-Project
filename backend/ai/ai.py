132083270390d53a4d39a0173ac2a9f5313eeb24
Emil Karlström <emil.karlstrom@gmail.com>
2021-04-20 11:25:11 +0200

    More template work for group 1

--- backend/ai/ai.py
+++ backend/ai/ai.py
@@ -15,6 +15,22 @@ def create_ai_model():
     """
     pass
 
+def save_ai_model(model, save_weights_path):
+        """
+            Saves the AI model's weights into a file
+
+        Args::
+            model: A tensorflow AI model
+            save_weights_path: str object representing the file path to save the weights to
+
+        Returns::
+            Tensorflow AI model
+
+        Raises::
+            Any errors tensorflow might've raised when loading weights
+    """
+    pass
+
 def train_ai(model, data):
     """
         Trains an AI model
