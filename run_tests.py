886b82dcf2fa12687e70d6f5ef10c9fce9343ef9
Emil Karlström <emil.karlstrom@gmail.com>
2021-04-27 15:00:02 +0200

    Whoops

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from tests import DatabaseUnitTest
+from tests import BackendUnitTest
+unittest.TestLoader.sortTestMethodsUsing = None
+
+unittest.main()
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
 from .test_database import DatabaseUnitTest
+from .test_backend import BackendUnitTest
