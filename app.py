34f0439ed530189c838e226860d9f04cfebc745c
Emil Karlström <emil.karlstrom@gmail.com>
2021-05-11 15:45:34 +0200

    Moved over to config files instead of command-line arguments, thought it would be more fitting

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import argparse
 from backend import create_app
 from backend import backend_app
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--ip', type=str, required=True,
-                help='The ip to run the Flask server on')
-parser.add_argument('--port', type=int, required=True,
-                help='The port to run the Flask server on')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('--ip', type=str, required=True,
+#                 help='The ip to run the Flask server on')
+# parser.add_argument('--port', type=int, required=True,
+#                 help='The port to run the Flask server on')
+# args = parser.parse_args()
 
 if __name__ == '__main__':
-    app = create_app(args.ip, args.port)
+    app = create_app('./config.ini')
     app.run()
     # Either or
     #backend_app.console_program(args.ip, args.port)
