869cd2c01582c2f17687beb3e33b6990bcd5c960
iTi1337 <64363659+iTi1337@users.noreply.github.com>
2021-05-19 15:25:16 +0200

    (added get_ai_names to init file)

--- ai/__init__.py
+++ ai/__init__.py
@@ -10,8 +10,4 @@ from .ai import run_ai
 
 from .ai import create_window
 
-# from .visualizer import run_sample
-
-# from .visualizer import visualize
-
-# from .visualizer import import_tf_special_dataset
+from .ai import get_ai_names
